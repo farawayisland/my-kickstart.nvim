86fd65e795a54636b46dfaef6ea492eeb301c0a0
farawayisland <207914612+farawayisland@users.noreply.github.com>
2026-04-14 19:21:18 +0700

    feat(autocommands): configure basic autocommands

--- lua/autocommands/basic.lua
+++ lua/autocommands/basic.lua
@@ -2,13 +2,58 @@
 -- [[ Basic autocommands ]]
 --  See `:help lua-guide-autocommands`
 
+-- Auto-create missing dirs when saving a file
+vim.api.nvim_create_autocmd('BufWritePre', {
+  desc = 'Auto-create missing dirs when saving a file',
+  group = vim.api.nvim_create_augroup('kickstart-auto-create-dir', { clear = true }),
+  pattern = '*',
+  callback = function()
+    local dir = vim.fn.expand '<afile>:p:h'
+    if vim.fn.isdirectory(dir) == 0 then vim.fn.mkdir(dir, 'p') end
+  end,
+})
+
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.hl.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function() vim.hl.on_yank() end,
+  callback = function() vim.hl.on_yank { timeout = 200 } end,
+})
+
+-- Restore cursor position on file open
+vim.api.nvim_create_autocmd('BufReadPost', {
+  desc = 'Restore cursor position on file open',
+  group = vim.api.nvim_create_augroup('kickstart-restore-cursor', { clear = true }),
+  pattern = '*',
+  callback = function()
+    local line = vim.fn.line '\'"'
+    if line > 1 and line <= vim.fn.line '$' then vim.cmd 'normal! g\'"' end
+  end,
+})
+
+-- Autosave and autoload folds
+--  Source: https://stackoverflow.com/a/77180744
+vim.api.nvim_create_autocmd('BufWinLeave', {
+  desc = 'Save view (folds), when closing file',
+  group = vim.api.nvim_create_augroup('save-folds', { clear = true }),
+  pattern = { '*.*' },
+  command = 'mkview',
+})
+
+vim.api.nvim_create_autocmd('BufWinEnter', {
+  desc = 'Load view (folds), when opening file',
+  group = vim.api.nvim_create_augroup('load-folds', { clear = true }),
+  pattern = { '*.*' },
+  command = 'silent! loadview',
+})
+
+-- Start insert mode when initializing a terminal buffer
+vim.api.nvim_create_autocmd('TermOpen', {
+  desc = 'Start insert mode when initializing a terminal buffer',
+  group = vim.api.nvim_create_augroup('startinsert-initialized-terminal-buffer', { clear = true }),
+  command = 'startinsert',
 })
 
 -- vim: et sts=2 sw=2 ts=2
