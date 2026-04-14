9eb083788a4e53e1f56d60980e470470ec813c20
farawayisland <207914612+farawayisland@users.noreply.github.com>
2026-04-15 02:56:17 +0700

    feat(plugins,lspconfig,telescope): change tools list and keymaps
    Update Mason-installed tools list and LSP-related keymaps.
    Source: https://github.com/oriori1703/kickstart-modular.nvim/blob/personal/lua/kickstart/plugins/lspconfig.lua.

--- lua/external-packages/non-ls.lua
+++ lua/external-packages/non-ls.lua
@@ -1,4 +1,15 @@
 -- ~/.config/nvims/kickstart/lua/external-packages/non-ls.lua
-return {}
+return {
+  'clang-format', -- C/C++ code formatter | https://clang.llvm.org/docs/ClangFormat.html
+  'google-java-format', -- Java code formatter | https://github.com/google/google-java-format
+  'markdownlint', -- Markdown/CommonMark style checker and linter | https://github.com/davidanson/markdownlint
+  'prettier', -- Multilingual code formatter | https://github.com/prettier/prettier
+  'prettierd', -- Multilingual code formatter | https://github.com/fsouza/prettierd
+  'shellharden', -- Bash code formatter | https://github.com/anordal/shellharden
+  'shfmt', -- Bash code formatter | https://github.com/mvdan/sh
+  'stylua', -- Lua code formatter | https://github.com/JohnnyMorganz/StyLua
+  'tex-fmt', -- LaTeX code formatter | https://github.com/WGUNDERWOOD/tex-fmt
+  'typstyle', -- Typst code formatter | https://typstyle-rs.github.io/typstyle
+}
 
 -- vim: et sts=2 sw=2 ts=2
