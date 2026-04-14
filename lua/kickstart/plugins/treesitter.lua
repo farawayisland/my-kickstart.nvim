9bbfd1c42484c65402913da4d6216e8786b9bf6e
farawayisland <207914612+farawayisland@users.noreply.github.com>
2026-04-15 02:03:31 +0700

    feat(plugins,treesitter): add parsers and disable `latex` parser

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -4,14 +4,96 @@
 return {
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
-    lazy = false,
-    build = ':TSUpdate',
     branch = 'main',
+    commit = 'ebc3201', -- Temporarily pins to parent commit of
+    -- https://github.com/nvim-treesitter/nvim-treesitter/commit/b9f9d692f1c7f0427502a3b84fa25d4bedb29ec1,
+    -- which includes breaking changes for
+    -- https://github.com/georgeharker/tree-sitter-zsh
+    build = ':TSUpdate',
+    lazy = false,
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
-    config = function()
-      -- ensure basic parser are installed
-      local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
-      require('nvim-treesitter').install(parsers)
+    ---@module 'nvim-treesitter'
+    ---@type TSConfig
+    opts = {
+      ensure_installed = {
+        'ada',
+        'agda',
+        'asm',
+        'awk',
+        'bash',
+        'c',
+        'cmake',
+        'commonlisp',
+        'cpp',
+        'css',
+        'csv',
+        'diff',
+        'editorconfig',
+        'fish',
+        'fortran',
+        'git_config',
+        'git_rebase',
+        'gitattributes',
+        'gitcommit',
+        'gitignore',
+        'glsl',
+        'go',
+        'haskell',
+        'html',
+        'java',
+        'javadoc',
+        'javascript',
+        'json',
+        'kotlin',
+        'llvm',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'nix',
+        'perl',
+        'php',
+        'php_only',
+        'phpdoc',
+        'powershell',
+        'python',
+        'query',
+        'r',
+        'ruby',
+        'rust',
+        'swift',
+        'toml',
+        'typescript',
+        'typst',
+        'vim',
+        'vimdoc',
+        'xml',
+        'yaml',
+        'zig',
+        'zsh',
+      },
+    },
+    config = function(_, opts)
+      -- install parsers
+      if opts.ensure_installed and #opts.ensure_installed > 0 then require('nvim-treesitter').install(opts.ensure_installed) end
+
+      -- Zsh-specific configuration
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'TSUpdate',
+        callback = function()
+          local installed_parsers = require('nvim-treesitter').get_installed 'parsers'
+          if vim.tbl_contains(installed_parsers, 'zsh') then
+            require('nvim-treesitter.parsers').zsh = {
+              install_info = {
+                'https://github.com/georgeharker/tree-sitter-zsh',
+                generate_from_json = false, -- only needed if repo does not contain `src/grammar.json` either
+                queries = 'nvim-queries', -- also install queries from given directory
+              },
+              tier = 3,
+            }
+          end
+        end,
+      })
 
       ---@param buf integer
       ---@param language string
@@ -40,7 +122,7 @@ return {
           local buf, filetype = args.buf, args.match
 
           local language = vim.treesitter.language.get_lang(filetype)
-          if not language then return end
+          if not language or language == 'latex' then return end
 
           local installed_parsers = require('nvim-treesitter').get_installed 'parsers'
 
@@ -59,4 +141,5 @@ return {
     end,
   },
 }
+
 -- vim: et sts=2 sw=2 ts=2
