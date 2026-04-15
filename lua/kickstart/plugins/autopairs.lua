8ac45a9946998ac11c9fc41a070bcc058c55439a
farawayisland <207914612+farawayisland@users.noreply.github.com>
2026-04-15 20:43:21 +0700

    feat(plugins,autopairs): configure `nvim-autopairs`

--- lua/kickstart/plugins/autopairs.lua
+++ lua/kickstart/plugins/autopairs.lua
@@ -8,4 +8,19 @@ return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
   opts = {},
+  config = function(_, opts)
+    require('nvim-autopairs').setup(opts)
+    local npairs = require 'nvim-autopairs'
+    local cond = require 'nvim-autopairs.conds'
+    local Rule = require 'nvim-autopairs.rule'
+    npairs.setup(opts)
+    npairs.add_rule(Rule('$', '$', { 'org', 'tex', 'latex' }):with_move(cond.done()))
+    npairs.add_rule(Rule('/', '/', { 'org' }):with_move(cond.done()))
+    npairs.add_rule(Rule('=', '=', { 'org' }):with_move(cond.done()))
+    npairs.add_rule(Rule('\\(', '\\)', { 'org', 'tex', 'latex' }))
+    npairs.add_rule(Rule('\\[', '\\]', { 'org', 'tex', 'latex' }))
+    npairs.add_rule(Rule('~', '~', { 'org' }):with_move(cond.done()))
+  end,
 }
+
+-- vim: et sts=2 sw=2 ts=2
